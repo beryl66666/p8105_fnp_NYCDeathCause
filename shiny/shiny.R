e29d5ab702b57cc672715828106051750bf6035b
sophienim <jc5924@cumc.columbia.edu>
2022-12-10 04:26:11 -0500

    update shiny

--- shiny/shiny.R
+++ shiny/shiny.R
@@ -15,7 +15,54 @@ complaint_select=nypd_complaint %>%
   mutate(
     date=str_c(year,month,day, sep = "-"),
     location=str_c(longitude,latitude,sep=", "))
+
+crime_type=complaint_select %>% 
+group_by(ofns_desc,year) %>% 
+  summarise(obs=n()) %>% 
+  filter(obs>5000)
+
 complaint_select
 
-ui= dashboardPage(
-  dashboardHeader(title = "NYPD Complaints During The Pandemic")
+fluidPage(
+  
+  titlePanel("NYPD Complaints During The Pandemic"),
+  sidebarPanel(
+    #dateRangeInput
+    dateRangeInput("dates", 
+                   label = h3("Date Range"),
+                   start = "2020-1-1",
+                   end   = "2021-9-9",
+                   format = "mm/dd/yy",
+                   separator = " - "),
+    #sliderInput
+    min_longitude=complaint_select %>% distinct(longitude) %>% min(),
+    max_longitude=complaint_select %>% distinct(longitude) %>% max(),
+    sliderInput("longitude", 
+                label = h3("Longitude"), 
+                min = min_longitude, 
+                max = max_longitude 
+                ),
+    #sliderInput
+    min_latitude=complaint_select %>% distinct(latitude) %>% min(),
+    max_latitude=complaint_select %>% distinct(latitude) %>% max(),
+    sliderInput("latitude", 
+                label = h3("Latitude"), 
+                min = min_latitude, 
+                max = max_latitude
+                ),
+    #checkboxInput
+    checkboxGroupInput("Crime_Type", 
+                       label = h3("Crime Type"), 
+                       choices = list("ASSAULT 3 & RELATED OFFENSES" = 1, 
+                                      "CRIMINAL MISCHIEF & RELATED OF" = 2, 
+                                      "GRAND LARCENY" = 3,
+                                      "HARRASSMENT 2" =4,
+                                      "PETIT LARCENY"=5
+                                      ),
+                       selected = 1),
+    
+    hr(),
+    fluidRow(column(3, verbatimTextOutput("value")))
+  )
+    )
+    
